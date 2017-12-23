24bf3599aedbac9790da416177e1006005ae5363
aripekka <honkanen.ap@gmail.com>
2017-12-23 15:52:30 +0200

    Added isotropic plate

---
+++ pyTTE/deformation.py
@@ -0,0 +1,30 @@
+from __future__ import division, print_function
+import numpy as np
+
+def isotropic_plate(Rx,Ry,nu,thickness):
+    '''
+    Creates a function for computing the Jacobian of
+    the displacement field for an isotropic plate.
+    '''
+
+    if Rx == 'inf' or Rx == 'Inf' or Rx == np.inf:
+        invRx = 0
+    else:
+        invRx = 1/Rx
+
+    if Ry == 'inf' or Ry == 'Inf' or Ry == np.inf:
+        invRy = 0
+    else:
+        invRy = 1/Ry
+
+    def jacobian(x,z):
+        ux_x = -(z+0.5*thickness)*invRx
+        ux_z = -x*invRx
+
+        uz_x = x*invRx
+        uz_z = nu/(1-nu)*(invRx+invRy)*(z+0.5*thickness)
+
+        return np.array([[ux_x,ux_y],[uy_x,uy_y]])
+
+    return jacobian
+
