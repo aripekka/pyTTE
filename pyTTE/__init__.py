96d25b4f36cc8dcb740e8591e4d8b10e7b33b326
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2019-07-15 17:19:24 +0300

    Added elastic_tensors to __init__

--- pyTTE/__init__.py
+++ pyTTE/__init__.py
@@ -1,4 +1,5 @@
 from .pyTTE import takagitaupin
 from . import deformation
+from . import elastic_tensors
 
-__all__=['takagitaupin','deformation']
+__all__=['takagitaupin','deformation','elastic_tensors']
