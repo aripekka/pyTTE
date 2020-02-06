d35e783fc2f1ed43e7a129920f66d5d9e842a946
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2020-02-06 19:40:59 +0200

    Modified the functions to return the rotation matrices

--- pyTTE/rotation_matrix.py
+++ pyTTE/rotation_matrix.py
@@ -1,11 +1,42 @@
 from __future__ import division, print_function
 import numpy as np
 
+
+def axis_angle(u,theta):
+    '''
+    Computes a matrix which performs a rotation of theta degrees counterclockwise about axis u.
+
+    Input:
+        u = 3-element vector giving the axis of rotation (does not need to be normalized)
+        theta = angle of counterclockwise rotation (in deg) 
+    Output:
+        R = 3x3 rotation matrix
+    '''    
+
+    #normalize
+    u = np.array(u)
+    u = u/np.sqrt(u[0]**2+u[1]**2+u[2]**2)
+    #rotation angle
+    th = np.radians(theta)
+
+    #rotation matrix
+    R=np.array([[        np.cos(th) + u[0]**2*(1-np.cos(th)), u[0]*u[1]*(1-np.cos(th)) - u[2]*np.sin(th), u[0]*u[2]*(1-np.cos(th)) + u[1]*np.sin(th)],
+                [ u[0]*u[1]*(1-np.cos(th)) + u[2]*np.sin(th),        np.cos(th) + u[1]**2*(1-np.cos(th)), u[1]*u[2]*(1-np.cos(th)) - u[0]*np.sin(th)],
+                [ u[0]*u[2]*(1-np.cos(th)) - u[1]*np.sin(th), u[1]*u[2]*(1-np.cos(th)) + u[0]*np.sin(th),        np.cos(th) + u[2]**2*(1-np.cos(th))]])
+
+    return R
+
+
 def align_vector_with_z_axis(h):
     '''
     Computes the rotation matrix which aligns the given vector along z-axis.
     For example, for reflection (hkl), h = h*b1 + k*b2 + l*b3, where bi
     are the primitive reciprocal vectors.
+
+    Input:
+        h = 3-element vector to be aligned
+    Output:
+        R = 3x3 rotation matrix
     '''
 
     if h[0] or h[1]:
@@ -30,79 +61,34 @@ def align_vector_with_z_axis(h):
 
     return R
 
-def rotation_matrix_axis_angle(u,theta):
-    '''
-        Computes a matrix which performs a rotation of theta degrees counterclockwise about axis u.
-    '''    
-    #normalize
-    u = np.array(u)
-    u = u/np.sqrt(u[0]**2+u[1]**2+u[2]**2)
-    #rotation angle
-    th = np.radians(theta)
 
-    #rotation matrix
-    R=np.array([[        np.cos(th) + u[0]**2*(1-np.cos(th)), u[0]*u[1]*(1-np.cos(th)) - u[2]*np.sin(th), u[0]*u[2]*(1-np.cos(th)) + u[1]*np.sin(th)],
-                [ u[0]*u[1]*(1-np.cos(th)) + u[2]*np.sin(th),        np.cos(th) + u[1]**2*(1-np.cos(th)), u[1]*u[2]*(1-np.cos(th)) - u[0]*np.sin(th)],
-                [ u[0]*u[2]*(1-np.cos(th)) - u[1]*np.sin(th), u[1]*u[2]*(1-np.cos(th)) + u[0]*np.sin(th),        np.cos(th) + u[2]**2*(1-np.cos(th))]])
+def inplane_rotation(alpha):
+    '''
+    Rotates the given tensor around the z-axis by alpha degrees counterclockwise.
+
+    Input:
+        alpha = angle of counterclockwise rotation (in deg) 
+    Output:
+        R = 3x3 rotation matrix
+    '''
+
+    R = rotation_matrix_axis_angle([0,0,1], alpha)
 
     return R
 
-def rotate_inplane(tensor, phi, x_dir = np.array([[1,0,0]]).T, y_dir = np.array([[0,1,0]]).T):
+def rotate_asymmetry(phi):
     '''
-        Rotates the given tensor around the z-axis by phi degrees counterclockwise.
-        x_dir and y_dir are the crystal directions (normalized hkl) along the x- and 
-        y-axes.
-    '''
+    Rotates the given tensor around the y-axis by phi degrees counterclockwise.
+    This corresponds to the definition of clockwise-positive asymmetry angle in
+    xz-plane as defined in the documentation.
 
-    #In-plane rotation
-    Q = rotation_matrix_axis_angle([0,0,1],phi)
-    for i in range(4):
-        tensor = np.tensordot(Q,tensor,axes=((1,),(i,)))
-
-    #calculate the crystal directions along the in-plane axes  
-
-    #this computes what were the directions of post-rotation x- and y axes 
-    #in terms of the pre-rotated x- and y- coordinates
-    prerot_x_coor = np.dot(Q.T, np.array([[1,0,0]]).T)
-    prerot_y_coor = np.dot(Q.T, np.array([[0,1,0]]).T)
-    
-    #prerotated x(y)-axis aligns with the crystal direction x(y)_dir
-    #and assuming that they are properly normalized, they form an orthonormal basis.
-    #Thus the postrotation x_dir and y_dir are linear combinations of
-    #prerot x_dir and y_dir based on the calculated coordinate transform:
-    new_x_dir = prerot_x_coor[0]*x_dir + prerot_x_coor[1]*y_dir
-    new_y_dir = prerot_y_coor[0]*x_dir + prerot_y_coor[1]*y_dir
-
-    return tensor, new_x_dir, new_y_dir
-
-def apply_asymmetry(tensor, phi, x_dir = np.array([[1,0,0]]).T, y_dir = np.array([[0,1,0]]).T):
-    '''
-        Rotates the given tensor around the y-axis by phi degrees counterclockwise.
-        This corresponds to the definition of clockwise-positive asymmetry angle in
-        xz-plane as defined in the documentation. x_dir and y_dir are the crystal 
-        directions (normalized hkl) along the x- and y-axes.
+    Input:
+        phi = asymmetry angle (in deg) 
+    Output:
+        R = 3x3 rotation matrix
     '''
 
-    #Asymmetric rotation (note that in pyTTE documentation
-    #rotation angle is positive in clockwise direction in
-    #right-handed xz-plane = counterclockwise rotation around y-axis)
-    Q = rotation_matrix_axis_angle([0,1,0],phi)
-    for i in range(4):
-        tensor = np.tensordot(Q,tensor,axes=((1,),(i,)))
+    R = rotation_matrix_axis_angle([0,1,0], phi)
 
-    #calculate the crystal directions along the in-plane axes  
-
-    #this computes what were the directions of post-rotation x- and y axes 
-    #in terms of the pre-rotated x- and y- coordinates
-    prerot_x_coor = np.dot(Q.T, np.array([[1,0,0]]).T)
-    prerot_y_coor = np.dot(Q.T, np.array([[0,1,0]]).T)
-    
-    #prerotated x(y)-axis aligns with the crystal direction x(y)_dir
-    #and assuming that they are properly normalized, they form an orthonormal basis.
-    #Thus the postrotation x_dir and y_dir are linear combinations of
-    #prerot x_dir and y_dir based on the calculated coordinate transform:
-    new_x_dir = prerot_x_coor[0]*x_dir + prerot_x_coor[1]*y_dir
-    new_y_dir = prerot_y_coor[0]*x_dir + prerot_y_coor[1]*y_dir
-
-    return tensor, new_x_dir, new_y_dir
+    return R
 
