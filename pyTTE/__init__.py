e43d266d5b3207871414738b2800b55d8f654827
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2020-03-05 17:02:48 +0200

    Updated the init

--- pyTTE/__init__.py
+++ pyTTE/__init__.py
@@ -1,5 +1,6 @@
-from .pyTTE import takagitaupin
+from .pyTTE import TakagiTaupin, TTcrystal, TTscan, takagitaupin 
 from . import deformation
 from . import elastic_tensors
+from .quantity import Quantity
 
-__all__=['takagitaupin','deformation','elastic_tensors']
+__all__ = ['TakagiTaupin', 'TTcrystal', 'TTscan', 'takagitaupin', 'deformation', 'elastic_tensors']
