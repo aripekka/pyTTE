Fixed the normalization of crystal directions

pyTTE/ttcrystal.py
@@ -470,7 +470,6 @@ class TTcrystal:
         bending radii.
         '''
 
-
         #calculate reciprocal vector of the diffraction hkl
         hkl = self.hkl[0]*self.reciprocal_primitives[:,0] +\
               self.hkl[1]*self.reciprocal_primitives[:,1] +\
@@ -536,9 +535,9 @@ class TTcrystal:
                'Asymmetry angle: ' + str(self.asymmetry)+'\n'+\
                'In-plane rotation angle: ' + str(self.in_plane_rotation)+'\n'+\
                'Crystal directions parallel to the Cartesian axes (after rotations):\n'+\
-               '    x || ' + np.array2string(self.crystal_directions[:,0]/self.crystal_directions[:,0].max(),precision=4,suppress_small=True)+'\n'+\
-               '    y || ' + np.array2string(self.crystal_directions[:,1]/self.crystal_directions[:,1].max(),precision=4,suppress_small=True)+'\n'+\
-               '    z || ' + np.array2string(self.crystal_directions[:,2]/self.crystal_directions[:,2].max(),precision=4,suppress_small=True)+'\n\n'+\
+               '    x || ' + np.array2string(self.crystal_directions[:,0]/np.abs(self.crystal_directions[:,0]).max(),precision=4,suppress_small=True)+'\n'+\
+               '    y || ' + np.array2string(self.crystal_directions[:,1]/np.abs(self.crystal_directions[:,1]).max(),precision=4,suppress_small=True)+'\n'+\
+               '    z || ' + np.array2string(self.crystal_directions[:,2]/np.abs(self.crystal_directions[:,2]).max(),precision=4,suppress_small=True)+'\n\n'+\
                'Meridional bending radius: ' + str(self.Rx) +'\n'+\
                'Sagittal bending radius: ' + str(self.Ry) +'\n\n'+\
                'Material elastic isotropy: ' + str(self.isotropy) +'\n' + elastic_str        