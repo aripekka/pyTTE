656e1c8f840cd00132c64ce2d239a57dc229030f
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2020-04-21 00:54:28 +0300

    Cleaned up the imports

--- pyTTE/pyTTE.py
+++ pyTTE/pyTTE.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import division, print_function
-import sys
-
-import numpy as np
-import matplotlib.pyplot as plt
-
 from scipy.integrate import ode
-
-from .quantity import Quantity
 from .ttcrystal import TTcrystal
+from .quantity import Quantity
 from .ttscan import TTscan
-
-import xraylib
-
+import matplotlib.pyplot as plt
 import multiprocess
+import numpy as np
+import xraylib
+import sys
 
 class TakagiTaupin:
 
